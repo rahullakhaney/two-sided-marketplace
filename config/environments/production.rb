7d7f32aa339facc74196f6d55928f6cca853a361
Rahul Lakhaney <rahul.lakhaney@gmail.com>
2015-10-27 21:26:32 +0530

    Added mailer info to production.rb for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.mandrillapp.com',
+    port: 587,
+    enable_starttls_auto: true,
+    user_name: ENV['MANDRILL_USER_NAME'],
+    password: ENV['MANDRILL_API_KEY'],
+    authentication: 'login'
+  }
 end
